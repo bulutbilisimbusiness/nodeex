3fa8b6d2baa26942fa413816d6dc5954db452811
erhan bulut <90501327+bulutbilisimbusiness@users.noreply.github.com>
2023-09-21 01:48:02 +0300

    ok

--- oop_3/index.js
+++ oop_3/index.js
@@ -18,3 +18,33 @@ res.end("<h1>Main Page</h1>")
 }
 }).listen(8000,()=>console.log("server runned at http://127.0.0.1:8000")) */
 
+http.createServer((req,res)=>{
+    if(req.url=='/'){
+        res.statusCode=404
+        res.statusMessage='Not found'
+
+        res.setHeader('Content-Type','text/html')
+        res.setHeader('another-header','another-value');
+
+        res.write('Satır1')
+        res.write('Satır2')
+        res.write('Satır3')
+        res.end()
+
+    } else if ( req.url == '/api' ){
+
+        if(req.method=='GET') {
+            res.writeHead(200,"Status Message",{
+                'Content-Type':'application/json',
+                'another-header':'another-value',
+            })
+            res.end('OK')
+        } else {
+            res.end('Not supporting different method')
+        }
+    }
+    else{
+        res.end('Server is running!')
+    }
+    
+}).listen(8000,()=>console.log('http://127.0.0.1:8000'))
