69ea5e26e30f7202451cd02dfe25de5255d49f30
erhan bulut <90501327+bulutbilisimbusiness@users.noreply.github.com>
2023-09-21 00:58:28 +0300

    node

---
+++ oop_3/index.js
@@ -0,0 +1,20 @@
+'use scrict'
+
+const http=require('node:http')
+
+/* const app=http.createServer((request,response)=>{
+    response.end('<h1>welcome to node server</h1>')
+
+})
+app.listen(8000,()=>console.log('Server runned :http://127.0.0.1:8000')) */
+/* 
+const app=http.createServer((req,res)=>{
+    if(req.url=='/'){
+res.end("<h1>Main Page</h1>")
+} else if(req.url=='/second'){
+    res.end("<h1>Second Page</h1>")
+} else{
+    res.end('<h1>Server is running..</h1>')
+}
+}).listen(8000,()=>console.log("server runned at http://127.0.0.1:8000")) */
+
